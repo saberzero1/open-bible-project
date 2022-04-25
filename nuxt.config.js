9a38573a27a0eee9dc794bbba18f811289ccd2e6
saberzero1 <ewjbangma@hotmail.com>
2022-04-25 23:30:53 +0200

    Added typescript config

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
