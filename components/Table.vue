e175db3262618160b155fa662f34ffcb09d8b359
saberzero1 <ewjbangma@hotmail.com>
2022-04-26 00:36:05 +0200

    Updated content

---
+++ components/Table.vue
@@ -0,0 +1,29 @@
+<template>
+    <table id="fresh-table" class="table">
+        <thead>
+            <th data-field="Link">Link</th>
+            <th data-field="Description">Description</th>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.url">
+                <td><a href="{{ item.url }}">{{ item.name }}</a></td>
+                <td>{{ item.description }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+    <script>
+    export default {
+        data() {
+            return {
+                items: []
+            }
+        },
+        async fetch() {
+            this.items = await fetch(
+                'links.json'
+            ).then(res => res.json())
+        }
+    }
+</script>
