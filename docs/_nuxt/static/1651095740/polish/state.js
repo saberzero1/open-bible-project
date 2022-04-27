72a3573a5097392fb903f7fc15ebeb82004235d8
saberzero1 <ewjbangma@hotmail.com>
2022-04-28 00:00:40 +0200

    deploy on gh-pages

--- docs/_nuxt/static/1651095740/polish/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651095740",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpolish",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4819a183"}}}}(null));
