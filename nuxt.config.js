f09650d40b9b73bd7f1de3b2a49d2a239a03d740
saberzero1 <ewjbangma@hotmail.com>
2022-04-27 18:00:52 +0200

    Updated workflow

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'open-bible-project',
